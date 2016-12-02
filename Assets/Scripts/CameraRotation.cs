e975536194f5aba7367de007d60707e97a03148a
Graham Park <parkmist@gmail.com>
2016-12-01 23:41:34 -0800

    Added Camera Rotation -GP
    added the camea rotation feature, and hopefully fixed the lighting in
    the build

---
+++ Assets/Scripts/CameraRotation.cs
@@ -0,0 +1,74 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public enum Direction
+{
+    pos1 = 0,
+    pos2,
+    pos3,
+    pos4
+}
+
+public class CameraRotation : MonoBehaviour {
+
+    public float mRotationSpeed;
+
+    public Direction mCamDirection;
+
+    int mCurrentDirection = 0;
+
+	void Start ()
+    {
+        mCurrentDirection = (int)mCamDirection;
+	}
+
+	void Update ()
+    {
+
+        //left
+        if (Input.GetKeyDown(KeyCode.H))
+        {
+            if(mCurrentDirection < 3)
+            {
+                mCurrentDirection++;
+            }else
+            {
+                mCurrentDirection = 0;
+            }
+            print(mCurrentDirection);
+            mCamDirection = (Direction)mCurrentDirection;
+        }
+
+        //right
+        if (Input.GetKeyDown(KeyCode.J))
+        {
+            if (mCurrentDirection > 0)
+            {
+                mCurrentDirection--;
+            }
+            else
+            {
+                mCurrentDirection = 3;
+            }
+            print(mCurrentDirection);
+            mCamDirection = (Direction)mCurrentDirection;
+        }
+
+        if (mCamDirection == Direction.pos1)
+        {
+            transform.rotation = Quaternion.RotateTowards(transform.rotation, Quaternion.AngleAxis(0, Vector3.up), mRotationSpeed * Time.deltaTime);
+        }
+        else if(mCamDirection == Direction.pos2)
+        {
+            transform.rotation = Quaternion.RotateTowards(transform.rotation, Quaternion.AngleAxis(90, Vector3.up), mRotationSpeed * Time.deltaTime);
+        }
+        else if (mCamDirection == Direction.pos3)
+        {
+            transform.rotation = Quaternion.RotateTowards(transform.rotation, Quaternion.AngleAxis(180, Vector3.up), mRotationSpeed * Time.deltaTime);
+        }
+        else if (mCamDirection == Direction.pos4)
+        {
+            transform.rotation = Quaternion.RotateTowards(transform.rotation, Quaternion.AngleAxis(270, Vector3.up), mRotationSpeed * Time.deltaTime);
+        }
+    }
+}

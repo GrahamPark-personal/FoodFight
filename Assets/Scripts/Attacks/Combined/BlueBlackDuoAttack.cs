7a28d6ca429eefba998b38df4178c1bb5fa85dc6
Graham Park <parkmist@gmail.com>
2017-02-28 19:50:15 -0800

    GP
    -static virus is done
    -graveyard is working, but has bugs
    -NecroMania and NA18 are working, but also have bugs
    -Visuals added

---
+++ Assets/Scripts/Attacks/Combined/BlueBlackDuoAttack.cs
@@ -0,0 +1,49 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BlueBlackDuoAttack : Attack
+{
+
+    public GameObject ObjectMinion;
+
+    public override void Init()
+    {
+
+        CreateID();
+        SetRange(1);
+        SetEffectDuration(2);
+
+        SetStartPos(GameManager.sInstance.mSelectedCell);
+
+        GameManager.sInstance.mAttackShape = AttackShape.AreaNoCharacters;
+        GameManager.sInstance.mCurrentRange = GetRange();
+
+
+    }
+
+    public override void Exit()
+    {
+
+    }
+
+    public override void Execute(IntVector2 pos)
+    {
+
+        Character mCharOnCell = GetComponent<Character>();
+
+        Cell myCell = GameManager.sInstance.mCurrGrid.rows[pos.y].cols[pos.x];
+
+        GameObject mMinion = Instantiate(ObjectMinion, myCell.transform.position + new Vector3(0, 1, 0), myCell.transform.rotation);
+
+        Character tempChar = mMinion.GetComponent<Character>();
+
+        mCharOnCell.SpawnedMinion1 = tempChar;
+        mCharOnCell.mHasSpawnedMinion = true;
+        mCharOnCell.AddAilment(AilmentID.SpawnMinion, GetEffectDuration(), 0);
+
+        myCell.mCharacterObj = tempChar;
+        myCell.mTypeOnCell = TypeOnCell.playerMinion;
+
+    }
+}

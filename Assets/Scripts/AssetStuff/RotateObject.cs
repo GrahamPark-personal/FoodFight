028b0e9893bdfda952f3f5bbb1be4912a328c96c
GrahamPark-personal <259907@AIV.EDMC.STU>
2017-06-01 22:37:08 -0700

    GP
    Models in

---
+++ Assets/Scripts/AssetStuff/RotateObject.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class RotateObject : MonoBehaviour
+{
+
+    public Vector3 rotation;
+    public float mSpeed;
+
+    void Update()
+    {
+        transform.Rotate(rotation * mSpeed * Time.deltaTime);
+    }
+}

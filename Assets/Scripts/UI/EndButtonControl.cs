8fda4f0439991655918dff1d8f48fe3a4465dc8e
GrahamPark-personal <0259907@AIV.EDMC.STU>
2017-09-10 15:34:23 -0700

    GP did stuff
    ...

---
+++ Assets/Scripts/UI/EndButtonControl.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class EndButtonControl : MonoBehaviour
+{
+    private RawImage mButtonImage;
+
+    void Start()
+    {
+        mButtonImage = GetComponent<RawImage>();
+        mButtonImage.enabled = false;
+    }
+
+    void Update()
+    {
+        if(GameManager.sInstance.GameLost)
+        {
+            mButtonImage.enabled = true;
+        }
+    }
+}
